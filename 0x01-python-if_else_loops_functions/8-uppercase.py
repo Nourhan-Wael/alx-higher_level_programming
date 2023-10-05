743583869023a6a0897d843006a77c57e1b9b810
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-05 09:29:07 +0300

    conditions and loop

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def uppercase(str):
     cn = 0
-    if str != None:
-        for i in str:
-            j = 0
-            cn = cn + 1
-            if ord(i) >= ord('a') and ord(i) <= ord('z'):
-                j = ord(i) - 32
-            else:
-                j = ord(i)
-            if (cn == len(str)):
-                print("{:c}".format(j))
-            else:
-                print("{:c}".format(j), end="")
+    for i in str:
+        j = 0
+        cn = cn + 1
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            j = ord(i) - 32
+        else:
+            j = ord(i)
+        if (cn == len(str)):
+            print("{:c}".format(j))
+        else:
+            print("{:c}".format(j), end="")
