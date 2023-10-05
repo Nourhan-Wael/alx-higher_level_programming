6e813e9ac12496183ed01fd6578ac2b48b88372f
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-05 06:14:44 +0300

    conditions and loop

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 99:
-        print("{}".format(i))
-    if i < 10:
-        print("0{}".format(i), end=",")
-    else:
-        print("{}".format(i), end=",")
+for i in range(0, 10):
+    for j in range(i+1, 10):
+        if i == 8 and j == 9:
+            print("{}".format(i), end="")
+            print("{}".format(j))
+        else:
+            print("{}".format(i), end="")
+            print("{}".format(j), end=", ")
