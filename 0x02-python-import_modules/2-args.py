ee4d63d0c2819e0e17b2633e8c02b179a19d6bde
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-09 01:06:10 +0300

    import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     if len(argv) == 2:
         print("{0} argument:".format(len(argv) - 1))
-        print("1: {}".format( argv[1]))
+        print("1: {}".format(argv[1]))
     elif len(argv) == 1:
         print("{0} arguments.".format(len(argv) - 1))
     else:
