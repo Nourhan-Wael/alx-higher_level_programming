e84d67e546c23964a96834f3c71e73981f3d67aa
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-15 22:58:32 +0300

    list function

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,10 +4,12 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(matrix)):
             if len(matrix[0]) > 1:
                 for j in range(len(matrix[0]) - 1):
-                     print("{:d}".format(matrix[i][j]), end=" ")
+                    print("{:d}".format(matrix[i][j]), end=" ")
                 print("{:d}".format(matrix[i][j + 1]), end="")
             else:
                 print("{:d}".format(matrix[i][0]), end="")
             print()
+    elif len(matrix) == 1 and len(matrix[0]) != 0:
+        print("{:d}".format(matrix[0][0]))
     else:
         print()
