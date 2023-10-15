716ab9cc69a48ce1cd507b405b3893437df2b155
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-16 01:14:55 +0300

    list function

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,9 +1,1 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    mx=my_list[0]
-    if len(my_list) == 0:
-        return (None)
-    for i in my_list:
-        if i >= mx:
-            mx = i
-    return (mx)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    mx=my_list[0]
+    if len(my_list) == 0:
+        return (None)
+    for i in my_list:
+        if i >= mx:
+            mx = i
+    return (mx)
