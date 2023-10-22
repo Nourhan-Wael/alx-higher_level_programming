943d96effcca3cde481537801e4270de4081e62d
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-23 01:17:17 +0300

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    mx = 0
+    if a_dictionary == None:
+        return None
+    for i in a_dictionary.keys():
+        if a_dictionary[i] > mx:
+            mx = a_dictionary[i]
+            k = i
+    return k
