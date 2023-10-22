43dcede116d84b6c017fc3fa7083919cfdb9cae5
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-23 01:01:45 +0300

    update

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    i = 0
+    k = list(a_dictionary.keys())
+    while i < len(k):
+        if k[i] == key:
+            del a_dictionary[key]
+        i += 1
+    return a_dictionary
