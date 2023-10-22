b207b85f87dd04b789f9d7d5bac721de127692ca
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-22 22:48:52 +0300

    update

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,10 @@ def uniq_add(my_list=[]):
     flag = False
     sum = 0
     i = 0
+    if len(my_list) == 0:
+        return 0
+    if len(my_list) == 1:
+        return my_list[0]
     while i < len(my_list):
         j = i + 1
         while j < len(my_list):
@@ -11,7 +15,7 @@ def uniq_add(my_list=[]):
             j += 1
         i += 1
     if my_list[0] == my_list[1]:
-                my_list.pop(1)
+        my_list.pop(1)
     for k in my_list:
         sum += k
     return sum
