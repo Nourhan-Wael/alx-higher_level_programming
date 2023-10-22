7b6cf338d2eb773eca5ce9eb4ba54a9f61ebe0cb
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-23 01:43:04 +0300

    update

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set_1 & set_2
+def complex_delete(a_dictionary, value):
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
