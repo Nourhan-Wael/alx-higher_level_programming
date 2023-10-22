cb3132d3eee95c601b7e5c6d7491948181a6384d
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-23 00:22:12 +0300

    update

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    keys = list(a_dictionary.keys())
+    keys.sort()
+    for i in keys:
+        print("{0} : {1}".format(i, a_dictionary[i]))
+
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
