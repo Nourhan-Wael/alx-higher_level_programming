556dbcafa57c516d4362cc7810a0c3d0f9b847f3
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-24 01:28:53 +0300

    update

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    cn = 0
+    for i in range(0, x):
+        try:
+             print("{:d}".format(my_list[i]), end="")
+             cn+= 1
+        except (TypeError, ValueError):
+            pass
+    print()
+    return cn
