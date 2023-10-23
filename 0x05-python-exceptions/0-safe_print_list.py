32826e65f816e6c16fac114746b41200f68a4bb4
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-24 01:11:51 +0300

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     cn = 0
-    if my_list:
-        try:
-            for i in range(0, x):
-                print(my_list[i], end="")
-                cn+= 1
-            print()
-        except Exception as e:
-            print()
+    try:
+        for i in range(0, x):
+            print(my_list[i], end="")
+            cn+= 1
+        print()
+    except:
+        print()
     return cn
