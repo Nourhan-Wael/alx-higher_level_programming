59ad4fc36697e51534835b1ce1dac1990b736718
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-25 15:24:50 +0300

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""square module."""
+
+
+class Square:
+    """square module."""
+    def __init__(self, size):
+        """constructor
+        Args:
+            size: length
+        """
+        self.__size = size
+
