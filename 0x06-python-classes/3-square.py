80352896769332062f986f93e33c6d492a919513
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-10-25 15:54:52 +0300

    update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""square module."""
+
+
+class Square:
+    """square module."""
+    def __init__(self, size = 0):
+        """constructor
+        Args:
+            size: length
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+        
+    def area(self):
+        """area
+        returns:
+            the area
+        """
+        return self.__size ** 2
